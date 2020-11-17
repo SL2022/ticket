b9adb7a40b539c70650eab2bca1649e89078a1a9
Walther Højgaard Diechmann <walther@LilleBukkeBruse.local>
2020-11-17 18:25:48 +0100

    added alpineJS + first Cypress test, YEAH!

--- test/ticket_web/live/page_live_test.exs
+++ test/ticket_web/live/page_live_test.exs
@@ -4,7 +4,7 @@ defmodule TicketWeb.PageLiveTest do
   import Phoenix.LiveViewTest
 
   test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/")
+    {:ok, page_live, disconnected_html} = live(conn, "/test_workbench")
     assert disconnected_html =~ "Welcome to Phoenix!"
     assert render(page_live) =~ "Welcome to Phoenix!"
   end
